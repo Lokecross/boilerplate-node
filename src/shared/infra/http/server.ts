d9730975988bab0e470a5df4e96d62dc04ffd121
loke <lucas.marcelino.silva@live.com>
2020-09-17 17:45:33 -0300

    feat: ddd

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,13 +3,14 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
 
-import AppError from './errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import routes from './routes';
 
-import './database';
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
